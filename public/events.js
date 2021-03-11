aca834861a8f931587e1e889c1a3146734872137
Natalie Oldroyd <natoh18@gmail.com>
2021-03-11 14:53:50 -0700

    errors

--- public/events.js
+++ public/events.js
@@ -1,17 +1,27 @@
  document.addEventListener('DOMContentLoaded', () => {
 const getKittenImg = async () => {
+    let loader = document.querySelector(".loader")
+    loader.innerHTML = "Loading..."
+
   const res = await fetch("/kitten/image");
   const json = await res.json();
   let catImg = document.querySelector(".cat-pic");
 
-  if (res.ok) {
-    console.log(res);
-    catImg.src = json.src;
-  } 
-  
+    if (res.ok) {
+        loader.innerHTML =null;
+        console.log(res);
+        catImg.src = json.src;
+      } else {
+          alert("Something went wrong! Please try again!")
+      }
+
+
+
+
+
   console.log(catImg.src);
 
-    
+
  }
 getKittenImg();
 
